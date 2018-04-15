073241f07b1795ca21f0710c88aedfc5b686e568
elder <elder.nakashima@gmail.com>
2018-04-15 20:39:57 -0300

    functional maps example with markers and http requests

--- app/src/main/java/com/example/enakashima/mapsexample/MapsActivity.java
+++ app/src/main/java/com/example/enakashima/mapsexample/MapsActivity.java
@@ -1,13 +1,19 @@
 package com.example.enakashima.mapsexample;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.turbomanage.httpclient.AsyncCallback;
 import com.turbomanage.httpclient.BasicHttpClient;
@@ -15,9 +21,14 @@ import com.turbomanage.httpclient.HttpResponse;
 import com.turbomanage.httpclient.ParameterMap;
 import com.turbomanage.httpclient.android.AndroidHttpClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button find;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        find = findViewById(R.id.find);
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GetRequest getRequest = new GetRequest();
+                getRequest.execute("http://7e78b5e5.ngrok.io/");
+            }
+        });
     }
 
 
@@ -43,23 +63,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-
-
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
-    public void loginDev(String userEmail) {
-        BasicHttpClient httpClient = new BasicHttpClient("http://localhost:8888");
-        ParameterMap params = httpClient.newParams()
-                .add("continue", "/")
-                .add("email", userEmail)
-                .add("action", "Log In");
-        httpClient.addHeader("name", "value");
-        httpClient.setConnectionTimeout(2000);
-        HttpResponse httpResponse = httpClient.post("/_ah/login", params);
+    private class GetRequest extends AsyncTask<String, Long, String> {
+        protected String doInBackground(String... urls) {
+            try {
+                HttpRequest request =  HttpRequest.get(urls[0]);
+                System.out.println("URL: " + urls[0]);
+                String response = "0";
+                if (request.ok()) {
+                    response = HttpRequest.get(urls[0]).body();
+                }
+                return response;
+            } catch (HttpRequest.HttpRequestException exception) {
+                return null;
+            }
+        }
+
+        protected void onProgressUpdate(Long... progress) {
+        }
+
+        protected void onPostExecute(String response) {
+            JSONArray karts = null;
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            try {
+                karts = new JSONArray(response);
+                for (int i = 0; i < karts.length(); i++) {
+                    JSONObject item = karts.getJSONObject(i);
+                    LatLng latLng = new LatLng(item.getDouble("lat"),item.getDouble("long"));
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(item.getString("name")));
+                    builder.include(latLng);
+                }
+                LatLngBounds bounds = builder.build();
+                int padding = 50; // offset from edges of the map in pixels
+                CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                mMap.animateCamera(cu);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
